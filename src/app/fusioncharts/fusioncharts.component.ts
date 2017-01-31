5b80cc1a7698e7088438c13b26e17a073bb443f7
Rohit Kumar <rohit.kr001@gmail.com>
2017-01-31 17:29:53 +0530

    Add sample implementation for Chart, PowerCharts, Widgets and Map

--- src/app/fusioncharts/fusioncharts.component.ts
+++ src/app/fusioncharts/fusioncharts.component.ts
@@ -1,23 +1,42 @@
 import {Component} from '@angular/core';
 
+import * as FusionCharts from 'fusioncharts';
+import * as PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+import * as Widgets from 'fusioncharts/fusioncharts.widgets';
+import * as Maps from 'fusioncharts/fusioncharts.maps';
+import * as World from 'fusioncharts/maps/fusioncharts.world';
+import * as fint from 'fusioncharts/themes/fusioncharts.theme.fint';
+
+PowerCharts(FusionCharts);
+Widgets(FusionCharts);
+Maps(FusionCharts);
+World(FusionCharts);
+
 var chartData = {
+  chart: {theme: 'fint'},
   data: [{value: 50}, {value: 55}, {value: 70}, {value: 60}]
 }
-var configObj = {
-  type: "Column2D",
-  width: 500,
-  height: 300,
-  dataFormat: 'json',
-  dataSource: {chart: {}, data: [{value: 500}, {value: 600}]}
+
+var config = {
+    type: "Column2D",
+    width: 500,
+    height: 300,
+    dataFormat: 'json',
+    dataSource: chartData
 }
 
 @Component({
   	selector: 'chart',
-  	template: '<fusioncharts [chartConfig] = configuration> Column2D chart will render here... </fusioncharts>'
+  	template: `
+              <fusioncharts [chartConfig]=chartConf> </fusioncharts>
+              <fusioncharts type="Spline" [chartConfig]="chartConf"> </fusioncharts>
+              <fusioncharts type="Pyramid" [chartConfig]=chartConf> </fusioncharts>
+              <fusioncharts type="World" [chartConfig]=chartConf> Column2D chart will render here... </fusioncharts>
+          `
 })
 
 export class FusionChartsSample {
-	myData = JSON.stringify(chartData);
+    chartData = JSON.stringify(chartData);
 
-  	configuration = JSON.stringify(configObj);
+    chartConf = JSON.stringify(config);
 }
