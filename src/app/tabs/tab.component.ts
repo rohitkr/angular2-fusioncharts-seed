21ee267d533b14865e2c63928715ca33e5138fba
Rohit Kumar <rohit.kr001@gmail.com>
2017-02-15 10:49:04 +0530

    Tab navigation using interface

--- src/app/tabs/tab.component.ts
+++ src/app/tabs/tab.component.ts
@@ -4,37 +4,33 @@ import { Tab } from './tabs.interface';
 import { TabsComponent } from './tabs.component';
 
 @Component({
-  selector: 'tab',
-  template: `
-<div [hidden]="!selected">
-  <ng-content></ng-content>
+    selector: 'tab',
+    template: `<div [hidden]="!selected">
+	<ng-content></ng-content>
 </div>
-  `
+`
 })
 export class TabComponent implements OnInit, Tab {
-  
-  @Input() tabTitle;
 
-  selected: boolean;
+    @Input() tabTitle;
 
+    selected: boolean;
 
-  constructor(private tabsComponent: TabsComponent) {}
-  
+    constructor(private tabsComponent: TabsComponent) {}
+
     ngOnInit() {
-      this.tabsComponent.addTab(this);
+        this.tabsComponent.addTab(this);
     }
 }
-
-
 
 
 @NgModule({
     imports: [ BrowserModule ],
     declarations: [
-        TabsComponent, TabComponent
+            TabsComponent, TabComponent
     ],
     exports: [
-        TabsComponent, TabComponent
+            TabsComponent, TabComponent
     ]
 })
 
